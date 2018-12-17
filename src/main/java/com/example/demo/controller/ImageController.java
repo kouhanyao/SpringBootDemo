4033e692816bb5bdf5caf9c9963870560a85e0ba
寇含尧 <kouhanyao@qiyego.com>
2018-12-17 11:23:17 +0800

    处理下载文件名字乱码问题

--- src/main/java/com/example/demo/controller/ImageController.java
+++ src/main/java/com/example/demo/controller/ImageController.java
@@ -6,19 +6,18 @@ import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.common.BitMatrix;
+import com.sun.xml.internal.messaging.saaj.packaging.mime.internet.MimeUtility;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.BufferedOutputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.HashMap;
@@ -102,7 +101,7 @@ public class ImageController {
     }
 
     @RequestMapping("/getQrcodeImageZip")
-    public void getQrcodeImageZip(HttpServletResponse response) throws Exception {
+    public void getQrcodeImageZip(HttpServletResponse response, HttpServletRequest request) throws Exception {
         Integer pixel = 200;
         String[] ss = {"一", "二"};
         ByteArrayOutputStream output = new ByteArrayOutputStream();
@@ -129,9 +128,66 @@ public class ImageController {
         out.close();
         OutputStream outputStream = new BufferedOutputStream(response.getOutputStream());
         response.setHeader("Content-Disposition", "attachment; filename=\"" + URLEncoder.encode("推荐人.zip", "UTF-8") + "\"");
+        //response.setHeader("Content-Disposition", String.format("attachment; %s", getDownLoadFileName("推荐人.zip",request)));
         response.setContentType("application/octet-stream;charset=UTF-8");
         outputStream.write(output.toByteArray());
         outputStream.flush();
         outputStream.close();
+    }
+
+    /**
+     * 处理下载文件名字乱码问题
+     * @param filename
+     * @param request
+     * @return
+     */
+    private String getDownLoadFileName(String filename,HttpServletRequest request) {
+        //Edge header  User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134
+        String new_filename = null;
+        try {
+            new_filename = URLEncoder.encode(filename, "UTF8");
+        } catch (UnsupportedEncodingException e1) {
+            e1.printStackTrace();
+        }
+        String userAgent = request.getHeader("User-Agent");
+        // System.out.println(userAgent);
+        String rtn = "filename=\"" + new_filename + "\"";
+        // 如果没有UA，则默认使用IE的方式进行编码，因为毕竟IE还是占多数的
+        if (userAgent != null) {
+            userAgent = userAgent.toLowerCase();
+            // IE、edge浏览器，只能采用URLEncoder编码
+            if (userAgent.indexOf("msie") != -1 || userAgent.indexOf("edge") !=-1) {
+                rtn = "filename=\"" + new_filename + "\"";
+            }
+            // Opera浏览器只能采用filename*
+            else if (userAgent.indexOf("opera") != -1) {
+                rtn = "filename*=UTF-8''" + new_filename;
+            }
+            // Safari浏览器，只能采用ISO编码的中文输出
+            else if (userAgent.indexOf("safari") != -1) {
+                try {
+                    rtn = "filename=\""
+                            + new String(filename.getBytes("UTF-8"),
+                            "ISO8859-1") + "\"";
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+            }
+            // Chrome浏览器，只能采用MimeUtility编码或ISO编码的中文输出
+            else if (userAgent.indexOf("applewebkit") != -1) {
+                try {
+                    new_filename = MimeUtility
+                            .encodeText(filename, "UTF8", "B");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                rtn = "filename=\"" + new_filename + "\"";
+            }
+            // FireFox浏览器，可以使用MimeUtility或filename*或ISO编码的中文输出
+            else if (userAgent.indexOf("mozilla") != -1) {
+                rtn = "filename*=UTF-8''" + new_filename;
+            }
+        }
+        return rtn.replace("+", "%20");
     }
 }
